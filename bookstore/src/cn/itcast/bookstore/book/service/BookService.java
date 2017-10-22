5f5a5209d3200875820c9218fe233f5ffb0d7a1d
Admin <Admin@10.130.161.137>
2017-10-22 09:38:23 +0800

    第一次提交

---
+++ bookstore/src/cn/itcast/bookstore/book/service/BookService.java
@@ -0,0 +1,47 @@
+package cn.itcast.bookstore.book.service;
+
+import java.util.List;
+
+import cn.itcast.bookstore.book.dao.BookDao;
+import cn.itcast.bookstore.book.domain.Book;
+
+public class BookService {
+	private BookDao bookDao = new BookDao();
+	
+	/**
+	 * 查询所有图书
+	 * @return
+	 */
+	public List<Book> findAll() {
+		return bookDao.findAll();
+	}
+
+	/**
+	 * 按分类查询图书
+	 * @param cid
+	 * @return
+	 */
+	public List<Book> findByCategory(String cid) {
+		return bookDao.findByCategory(cid);
+	}
+
+	public Book load(String bid) {
+		return bookDao.findByBid(bid);
+	}
+
+	/**
+	 * 　添加图书
+	 * @param book
+	 */
+	public void add(Book book) {
+		bookDao.add(book);
+	}
+	
+	public void delete(String bid) {
+		bookDao.delete(bid);
+	}
+
+	public void edit(Book book) {
+		bookDao.edit(book);
+	}
+}
